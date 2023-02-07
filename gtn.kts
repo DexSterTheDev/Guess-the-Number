70e3a6239e31f299a1b4a18c0be7a48bed30378f
Basit Hameed <56188490+DexSterTheDev@users.noreply.github.com>
2023-02-07 11:06:55 +0500

    Update gtn.kts

--- gtn.kts
+++ gtn.kts
@@ -16,7 +16,6 @@ fun main() {
 
     println("You win! The number was $random")
 }
-In this code, a random number is generated using the Random class and stored in the random variable. The user is then prompted to guess the number using the readLine function. If the user's guess is too low, the program outputs "Too low". If the user's guess is too high, the program outputs "Too high". The loop continues until the user correctly guesses the number, at which point the program outputs "You win! The number was $random".
 
 
 
